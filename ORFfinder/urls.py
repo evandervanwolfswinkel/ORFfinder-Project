28000f1fca8b85d1abd981805475a15d11883acf
evandervanwolfswinkel <evandervanwolfswinkel@outlook.com>
2019-03-26 16:13:25 +0100

    Created url's for webpages

--- ORFfinder/urls.py
+++ ORFfinder/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ORFresult/', views.ORFresult, name='ORFresult')
+    path('ORFresult/', views.ORFresult, name='ORFresult'),
+    path('Genefunction/', views.gene_function, name='gene_function'),
+    path('Savedsequences/', views.saved_sequences, name='saved_sequences'),
+    path('About/', views.about, name='about')
+
+
 ]
--- ORFfinder/views.py
+++ ORFfinder/views.py
@@ -3,13 +3,26 @@ from django.shortcuts import render
 import ORFSearch
 
 
-# Create your views here.
 def index(request):
     return render(request, "index.html")
 
 
+def gene_function(request):
+    return render(request, "gene_function.html")
+
+
+def saved_sequences(request):
+    return render(request, "saved_sequences.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
 def ORFresult(request):
     rawsequence = request.GET.get('rawsequence')
     orf_list = ORFSearch.calculateORF(str(rawsequence))
+    return render(request, "ORFresult.html", {'orf_list': orf_list})
 
-    return render(request, "test.html", {'orf_list': orf_list})
+
+def Blastresult(request):
+    return render(request, "BLASTresult.html")
