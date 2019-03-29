2cd34a6e4f17185188fab6308146a5cf9773d66b
JungHoLoos <31917836+JungHoLoos@users.noreply.github.com>
2019-03-29 14:51:45 +0100

    Update SearchGenefunction.py

--- SearchGenefunction.py
+++ SearchGenefunction.py
@@ -7,35 +7,26 @@ from Bio.Blast import NCBIXML
 from BLASTobject import blast
 
 
-# Auteur: Jung Ho Loos, Evander van Wolfswinkel
-# Created: 27-3-2019
-# Functionality: Blasts both raw fasta input and ORF objects list
-# input and returns the information in blast objects list
-# Known Bugs: No known Bugs
-
-# Blasts raw fasta input, saves it to temporary file and then
-# Uses NCBIWWW.qblast to blast for gene function
-# Returns: Blast object list
-def BLASTraw(input):
+def BLASTraw(input):                                #input is a raw sequence
     if os.path.exists("tempfasta.fa"):
-        os.remove("tempfasta.fa")
+        os.remove("tempfasta.fa")                   #removes existing tempfasta file
     if os.path.exists("my_blast_result.xml"):
-        os.remove("my_blast_result.xml")
-    blast_result_list = []
-    E_value_thresh = 0.04
-    fasta = input
+        os.remove("my_blast_result.xml")            #removes existing my_blast_result.xml file
+    blast_result_list = []                          
+    E_value_thresh = 0.04                           #chosen E-value           
+    fasta = input                                   #create a new fasta file
     fastaformat = StringIO(fasta)
     with open("tempfasta.fa", 'a') as fastafile:
         fastafile.write(fastaformat.getvalue())
-    fasta_string = open("tempfasta.fa").read()
-    result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
+    fasta_string = open("tempfasta.fa").read()      #reads created fasta file
+    result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)    #BLAST using the fastafile
     print("done")
-    with open("my_blast_result.xml", "w") as out_handle:
+    with open("my_blast_result.xml", "w") as out_handle:            #saves BLAST output as XML file, my_blast_result.xml
         out_handle.write(result_handle.read())
     result_handle.close()
-    result_handle = open("my_blast_result.xml", 'r')
+    result_handle = open("my_blast_result.xml", 'r')                #reads the XML file
     blast_records = NCBIXML.parse(result_handle)
-    for blast_record in blast_records:
+    for blast_record in blast_records:                              #creates blast objects and saves them in the blast_result_list
         for alignment in blast_record.alignments:
             for hsp in alignment.hsps:
                 if hsp.expect < E_value_thresh:
@@ -45,20 +36,17 @@ def BLASTraw(input):
                     blastresult = blast(sequenceName, length, Evalue)
                     blast_result_list.append(blastresult)
 
-    return blast_result_list
+    return blast_result_list            #returns blast_result_list as output
 
 
-# Blasts orf object list input, saves it to temporary file and then
-# Uses NCBIWWW.qblast to blast for gene function
-# Returns: Blast object list
-def BLASTorf(input):
+def BLASTorf(input):    #input a list of ORF_objects 
     if os.path.exists("tempfasta.fa"):
-        os.remove("tempfasta.fa")
+        os.remove("tempfasta.fa")               #removes existing tempfasta file
     if os.path.exists("my_blast_result.xml"):
-        os.remove("my_blast_result.xml")
-    blast_result_list = []
-    E_value_thresh = 0.04
-    fasta = ""
+        os.remove("my_blast_result.xml")        #removes existing my_blast_result.sml file
+    blast_result_list = []                      
+    E_value_thresh = 0.04                       #chosen E-value
+    fasta = ""                                  #create a new fasta file                
     for ORF in input:
         ORFseq = ORF.get_sequence()
         ORFheader = ORF.get_header()
@@ -67,16 +55,15 @@ def BLASTorf(input):
     fastaformat = StringIO(fasta)
     with open("tempfasta.fa", 'a') as fastafile:
         fastafile.write(fastaformat.getvalue())
-    fasta_string = open("tempfasta.fa").read()
-    result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)
+    fasta_string = open("tempfasta.fa").read()          #reads created fasta file
+    result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string)    #BLAST using the fastafile
     print("done")
-    with open("my_blast_result.xml", "w") as out_handle:
-        out_handle.write(result_handle.read())
+    with open("my_blast_result.xml", "w") as out_handle:        #saves BLAST output as XML file, my_blast_result.xml
+        out_handle.write(result_handle.read())      
     result_handle.close()
-    result_handle = open("my_blast_result.xml", 'r')
+    result_handle = open("my_blast_result.xml", 'r')            #reads the XML file
     blast_records = NCBIXML.parse(result_handle)
-    for blast_record in blast_records:
-        for alignment in blast_record.alignments:
+    for blast_record in blast_records:                           #creates blast objects and saves them in the blast_result_list
             for hsp in alignment.hsps:
                 if hsp.expect < E_value_thresh:
                     sequenceName = alignment.title
@@ -85,4 +72,4 @@ def BLASTorf(input):
                     blastresult = blast(sequenceName, length, Evalue)
                     blast_result_list.append(blastresult)
 
-    return blast_result_list
+    return blast_result_list            #returns blast_result_list as output
